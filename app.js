949ee43e5f569acd7ce2746d86cd240bee75038f
Julien ROTT <julien.rott@gmail.com>
2012-11-08 10:10:27 +0100

    divers

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ var server = http.createServer(app);
 var sock;
 
 var dbstring = (process.env.NODE_ENV === 'production') ? url.parse(process.env.MONGOHQ_URL): "";
+console.log(dbstring);
 var dbhost = (process.env.NODE_ENV === 'production') ? dbstring.hostname : 'localhost';
 var dbport = (process.env.NODE_ENV === 'production') ? dbstring.port : 27017;
 
